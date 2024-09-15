59260085f135fee9e2bae2104b2c73659a8e6957
yolovi <yolandalopevi@gmail.com>
2024-09-16 01:43:20 +0200

    provisional login no logic implemented yet only structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { inter } from "./ui/fonts";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+//const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
