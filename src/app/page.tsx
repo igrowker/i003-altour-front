57de52151a6fc55c0c52eab5f7dbe28109107780
melodycious <melody.callejas@gmail.com>
2024-09-23 18:51:42 +0200

    Merged latest changes of develop with carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,48 @@
 import Image from "next/image";
 import altour from '../../public/altour.png';
 import { waffleSoft, lato } from "./ui/fonts";
-import Link from "next/link";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
-
-
+import Carrusel from "./ui/carrousel";
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  let recomendaciones = [
+    { id: 1, nombre: "La Sagrada Familia", descripcion: "La obra maestra inacabada de Antoni Gaudí es un icono mundial. Esta basílica, con sus torres y fachadas repletas de detalles, es una muestra impresionante del modernismo catalán. Cada rincón de la Sagrada Familia es una sorpresa visual, desde sus coloridas vidrieras hasta sus formas orgánicas inspiradas en la naturaleza)" },
+    { id: 2, nombre: "Park Güell", descripcion: "Otro de los tesoros de Gaudí, el Park Güell es un parque público con un diseño extravagante y lleno de color. Sus mosaicos, bancos sinuosos y vistas panorámicas de la ciudad lo convierten en un lugar mágico para pasear y relajarse. El famoso dragón y la sala hipóstila son dos de sus puntos más destacados." },
+    { id: 3, nombre: "Casa Batlló", descripcion: "Ubicada en el Paseo de Gracia, esta casa modernista es una de las obras más originales de Gaudí. Su fachada ondulante, revestida de cerámica de colores, y su interior lleno de simbolismo la convierten en una experiencia única. La Casa Batlló es como un cuento de hadas hecho realidad." },
+    { id: 4, nombre: "Barrio Gótico", descripcion: "El corazón histórico de Barcelona, el Barrio Gótico, es un laberinto de callejuelas estrechas, plazas encantadoras y edificios medievales. Aquí puedes encontrar la Catedral de Barcelona, la Plaça Sant Jaume y el Palau Reial Major, entre otros monumentos. Perderse por sus callejuelas es una forma maravillosa de sumergirse en la historia de la ciudad." },
+    { id: 5, nombre: "Barrio Gótico", descripcion: "El corazón histórico de Barcelona, el Barrio Gótico, es un laberinto de callejuelas estrechas, plazas encantadoras y edificios medievales. Aquí puedes encontrar la Catedral de Barcelona, la Plaça Sant Jaume y el Palau Reial Major, entre otros monumentos. Perderse por sus callejuelas es una forma maravillosa de sumergirse en la historia de la ciudad." },
+    { id: 6, nombre: "Barrio Gótico", descripcion: "El corazón histórico de Barcelona, el Barrio Gótico, es un laberinto de callejuelas estrechas, plazas encantadoras y edificios medievales. Aquí puedes encontrar la Catedral de Barcelona, la Plaça Sant Jaume y el Palau Reial Major, entre otros monumentos. Perderse por sus callejuelas es una forma maravillosa de sumergirse en la historia de la ciudad." }
+  ]
+  let categorias = [
+    { id: 1, nombre: "Historia", descripcion: "Lugares inolvidables para la historia y para tí" },
+    { id: 2, nombre: "Comida", descripcion: "Lo mejor de la gastronomía" }
+  ]
 
- 
-    <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-      <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <p
-          className={`${lato} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-        >
-          <Image src={altour} alt="Logo de mi aplicación" width={300} height={100} />
-          <strong>Welcome to Altour.</strong> 
-        </p>
-        <Link
-          href="/login"
-          className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-        >
-          <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-        </Link>
-        </div>
+  return (
+    <main className="flex min-h-screen flex-col p-6 pb-12 mb-7">
+    <div className="flex justify-center text-3xl">
+      <h1>Bienvenido/a Usuario</h1>
     </div>
+    <div className="flex justify-center mt-5">
+      <div className="basis-1/6 flex justify-start items-center">
+        <p className="text-2xl pl-4">Mapa?</p>
+      </div>
+      <div className="basis-5/6 flex justify-center items-center">
+        <Image src="/isotipo_altour.png" alt="altour logo" width={300} height={200} />
+      </div>
+    </div>
+    <div>
+      <h2 className="text-3xl pl-4 mb-2">Recomendaciones</h2>
+      <Carrusel slides={recomendaciones} />
+
+    </div>
+    <div>
+      <h2 className="text-3xl pl-4 mb-2">Categorías</h2>
+      <Carrusel slides={categorias} />
+
+    </div>
+
+
+
   </main>
   );
 }
