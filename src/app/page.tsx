9f0006d149418ce5cdfe59235996fba012d39def
Sebastian <sebas33696@gmail.com>
2024-09-23 16:39:22 +0200

    agregado carrusel de categorias y modificado componente para ser reutilizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,43 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
-import CarruselRecomendaciones from "./ui/carrousel";
+import Carrusel from "./ui/carrousel";
 export default function Home() {
   let recomendaciones = [
     { id: 1, nombre: "La Sagrada Familia", descripcion: "La obra maestra inacabada de Antoni Gaudí es un icono mundial. Esta basílica, con sus torres y fachadas repletas de detalles, es una muestra impresionante del modernismo catalán. Cada rincón de la Sagrada Familia es una sorpresa visual, desde sus coloridas vidrieras hasta sus formas orgánicas inspiradas en la naturaleza)" },
     { id: 2, nombre: "Park Güell", descripcion: "Otro de los tesoros de Gaudí, el Park Güell es un parque público con un diseño extravagante y lleno de color. Sus mosaicos, bancos sinuosos y vistas panorámicas de la ciudad lo convierten en un lugar mágico para pasear y relajarse. El famoso dragón y la sala hipóstila son dos de sus puntos más destacados." },
     { id: 3, nombre: "Casa Batlló", descripcion: "Ubicada en el Paseo de Gracia, esta casa modernista es una de las obras más originales de Gaudí. Su fachada ondulante, revestida de cerámica de colores, y su interior lleno de simbolismo la convierten en una experiencia única. La Casa Batlló es como un cuento de hadas hecho realidad." },
-    { id: 4, nombre: "Barrio Gótico", descripcion: "El corazón histórico de Barcelona, el Barrio Gótico, es un laberinto de callejuelas estrechas, plazas encantadoras y edificios medievales. Aquí puedes encontrar la Catedral de Barcelona, la Plaça Sant Jaume y el Palau Reial Major, entre otros monumentos. Perderse por sus callejuelas es una forma maravillosa de sumergirse en la historia de la ciudad." }
+    { id: 4, nombre: "Barrio Gótico", descripcion: "El corazón histórico de Barcelona, el Barrio Gótico, es un laberinto de callejuelas estrechas, plazas encantadoras y edificios medievales. Aquí puedes encontrar la Catedral de Barcelona, la Plaça Sant Jaume y el Palau Reial Major, entre otros monumentos. Perderse por sus callejuelas es una forma maravillosa de sumergirse en la historia de la ciudad." },
+    { id: 5, nombre: "Barrio Gótico", descripcion: "El corazón histórico de Barcelona, el Barrio Gótico, es un laberinto de callejuelas estrechas, plazas encantadoras y edificios medievales. Aquí puedes encontrar la Catedral de Barcelona, la Plaça Sant Jaume y el Palau Reial Major, entre otros monumentos. Perderse por sus callejuelas es una forma maravillosa de sumergirse en la historia de la ciudad." },
+    { id: 6, nombre: "Barrio Gótico", descripcion: "El corazón histórico de Barcelona, el Barrio Gótico, es un laberinto de callejuelas estrechas, plazas encantadoras y edificios medievales. Aquí puedes encontrar la Catedral de Barcelona, la Plaça Sant Jaume y el Palau Reial Major, entre otros monumentos. Perderse por sus callejuelas es una forma maravillosa de sumergirse en la historia de la ciudad." }
+  ]
+  let categorias = [
+    { id: 1, nombre: "Historia", descripcion: "Lugares inolvidables para la historia y para tí" },
+    { id: 2, nombre: "Comida", descripcion: "Lo mejor de la gastronomía" }
   ]
 
   return (
-    <main className="flex min-h-screen flex-col p-6 pb-10">
+    <main className="flex min-h-screen flex-col p-6 pb-12 mb-7">
       <div className="flex justify-center text-3xl">
         <h1>Bienvenido/a Usuario</h1>
       </div>
       <div className="flex justify-center mt-5">
         <div className="basis-1/6 flex justify-start items-center">
-          <p className="text-2xl">Mapa?</p>
+          <p className="text-2xl pl-4">Mapa?</p>
         </div>
         <div className="basis-5/6 flex justify-center items-center">
           <Image src="/isotipo_altour.png" alt="altour logo" width={300} height={200} />
         </div>
       </div>
       <div>
-        <h2 className="text-3xl">Recomendaciones</h2>
-        <div className="flex flex-wrap justify-between">
-          <CarruselRecomendaciones recomendaciones={recomendaciones} />
-        </div>
+        <h2 className="text-3xl pl-4 mb-2">Recomendaciones</h2>
+        <Carrusel slides={recomendaciones} />
+
+      </div>
+      <div>
+        <h2 className="text-3xl pl-4 mb-2">Categorías</h2>
+        <Carrusel slides={categorias} />
+
       </div>
 
 
--- src/app/ui/carrousel.tsx
+++ src/app/ui/carrousel.tsx
@@ -1,51 +1,90 @@
-"use client"
+"use client";
 import React, { useState } from 'react';
-import Image from 'next/image'; // Si usas Next.js
+import Image from 'next/image'; 
 
-interface Recomendacion {
+interface Slide {
     id: number;
     nombre: string;
     descripcion: string;
-  }
+}
 
-  interface CarruselRecomendacionesProps {
-    recomendaciones: Recomendacion[];
-  }
+interface CarruselProps {
+    slides: Slide[];
+}
 
-const CarruselRecomendaciones:React.FC<CarruselRecomendacionesProps> =     ({ recomendaciones }) => {
+const Carrusel: React.FC<CarruselProps> = ({ slides }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const totalSlides = recomendaciones.length;
+
+    const slidesToShow = 4;
+    const slidesToShowMedium = 2;  
+    const slidesToShowSmall = 1; 
+
+    const totalSlides = slides.length;
+
+    const getSlidesToShow = () => {
+        if (typeof window !== "undefined") {
+            if (window.innerWidth <= 640) {
+                return slidesToShowSmall;
+            } else if (window.innerWidth <= 1024) {
+                return slidesToShowMedium;
+            } else {
+                return slidesToShow; 
+            }
+        }
+        return slidesToShow; 
+    };
 
     const handlePrevClick = () => {
-        setCurrentIndex(currentIndex === 0 ? totalSlides - 1 : currentIndex - 1);
+        const visibleSlides = getSlidesToShow();
+        setCurrentIndex(currentIndex === 0 ? totalSlides - visibleSlides : currentIndex - visibleSlides);
     };
 
     const handleNextClick = () => {
-        setCurrentIndex(currentIndex === totalSlides - 1 ? 0 : currentIndex + 1);
+        const visibleSlides = getSlidesToShow();
+        setCurrentIndex((currentIndex + visibleSlides) % totalSlides);
     };
 
     return (
-        <div className="relative w-full max-w-lg mx-auto overflow-hidden"> {/* Aseguramos overflow-hidden */}
+        <div className="relative w-full max-w-5xl mx-auto overflow-hidden"> 
             {/* Carrusel */}
             <div
                 className="flex transition-transform duration-700 ease-in-out"
-                style={{ transform: `translateX(-${currentIndex * 100}%)` }} // Movemos las diapositivas
+                style={{ transform: `translateX(-${(currentIndex / getSlidesToShow()) * 100}%)` }} 
             >
-                {recomendaciones.map((recomendacion) => (
-                    <div key={recomendacion.id} className="w-full flex-shrink-0"> {/* Cada slide ocupa el 100% del ancho */}
-                        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto">
+                {slides.map((slide) => (
+                    <div 
+                        key={slide.id} 
+                        className="w-full sm:w-1/2 lg:w-1/4 flex-shrink-0 px-2"
+                    > {/* Cambiar el ancho según el tamaño de la pantalla */}
+                        <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                             <a href="#" className="flex justify-center">
                                 <Image src="/isotipo_altour.png" alt="altour logo" width={150} height={200} />
                             </a>
                             <div className="p-5">
                                 <a href="#">
-                                    <h5 className="mb-2 text-l font-bold tracking-tight text-gray-900 dark:text-white">{recomendacion.nombre}</h5>
+                                    <h5 className="mb-2 text-l font-bold tracking-tight text-gray-900 dark:text-white">
+                                        {slide.nombre}
+                                    </h5>
                                 </a>
-                                <p className="mb-3 font-normal text-xs text-gray-700 dark:text-gray-400">{recomendacion.descripcion}</p>
+                                <p className="mb-3 font-normal text-xs text-gray-700 dark:text-gray-400">
+                                    {slide.descripcion}
+                                </p>
                                 <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Ver más
-                                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                    <svg
+                                        className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 14 10"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                                        />
                                     </svg>
                                 </a>
                             </div>
@@ -55,14 +94,20 @@ const CarruselRecomendaciones:React.FC<CarruselRecomendacionesProps> =     ({ re
             </div>
 
             {/* Botones de navegación */}
-            <button onClick={handlePrevClick} className="absolute top-1/2 left-0 transform -translate-y-1/2 text-black px-2 py-2">
+            <button 
+                onClick={handlePrevClick} 
+                className="absolute top-1/2 left-0 transform -translate-y-1/2  text-black px-4 py-2 rounded-full"
+            >
                 ‹
             </button>
-            <button onClick={handleNextClick} className="absolute top-1/2 right-0 transform -translate-y-1/2 text-black px-2 py-2">
+            <button 
+                onClick={handleNextClick} 
+                className="absolute top-1/2 right-0 transform -translate-y-1/2  text-black px-4 py-2 rounded-full"
+            >
                 ›
             </button>
         </div>
     );
 };
 
-export default CarruselRecomendaciones;
+export default Carrusel;
