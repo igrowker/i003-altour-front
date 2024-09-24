d096a0867a6e115ad8765584827864ab6c7d1859
jomuarribas <jomuarribas@gmail.com>
2024-09-24 20:52:26 +0200

    Added props to the heatmap component for customization.

--- src/app/(private_routes)/heatmap/page.tsx
+++ src/app/(private_routes)/heatmap/page.tsx
@@ -3,7 +3,10 @@ import Heatmap from "@/app/components/Heatmap/Heatmap";
 export default function HeatmapPage() {
   return (
     <main>
-      <Heatmap />
+      <Heatmap 
+        searchAndCard={true}
+        containerStyle={{ width: '100%', height: 'calc(100svh - 145px)' }} 
+      />
     </main>
   );
 }
