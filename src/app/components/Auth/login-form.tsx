0a458388022f686867d70462325529b36b30b995
yolovi <yolandalopevi@gmail.com>
2024-10-08 00:39:54 +0200

    modified navbar, eliminated guards pages, redirect to correct pages after register and login

--- src/app/components/Auth/login-form.tsx
+++ src/app/components/Auth/login-form.tsx
@@ -31,16 +31,14 @@ interface ErrorState {
 }
 
 // prop onlogin para que active el onboarding
-interface LoginFormProps {
-  
-}
+interface LoginFormProps {}
 
 export default function LoginForm() {
   //recoge el estado de la sesion de next.js del usuario (si está logueado o no)
   const { data: session } = useSession();
   console.log("session", session);
-  const sessionToken = session?.accessToken 
-  console.log("sessionToken", typeof sessionToken )
+  const sessionToken = session?.accessToken;
+  console.log("sessionToken", typeof sessionToken);
 
   //Credentials Auth en un estado
   const [formState, setFormState] = useState<FormState>({
@@ -84,20 +82,18 @@ export default function LoginForm() {
 
     setLoading(false);
 
-    console.log(result)
+    console.log(result);
 
     if (result && !result.error) {
-      router.push("/onboarding"); 
+      router.push("/onboarding");
     } else {
       setError({ ...error, email: "Correo o contraseña inválidos" });
     }
-
-
   };
 
   // Autenticación con Google
   const handleGoogleSignIn = async () => {
-    await signIn("google", { callbackUrl: "/" });
+    await signIn("google", { callbackUrl: "/home" });
   };
 
   return (
